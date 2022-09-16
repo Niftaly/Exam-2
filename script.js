a9916fe0c75c64bbd6e3d4a7edee2509801e8ca2
Niftaly <108418135+Niftaly@users.noreply.github.com>
2022-09-16 22:40:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let random500 = (Math.ceil(Math.random() * 500));
+
+
+function Click() {
+    let randomNumber = document.getElementById('cavab').value;
+
+    if (0 < randomNumber < 10) {
+        alert('Avw004!!')
+    } else if (11 < randomNumber < 100) {
+        alert('Agh0Ja0Ba4Di')
+    } else {+
+        alert('Keb0ir0l4i!')
+    }
+
+}
